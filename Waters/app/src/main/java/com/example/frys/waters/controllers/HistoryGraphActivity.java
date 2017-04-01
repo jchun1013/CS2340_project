ac7dec60695bcd2c78dbf179875ee0c8c3fcbde0
EllaKim0415 <khrstar7979@gmail.com>
2017-04-01 18:34:21 -0400

    axis label + map

--- Waters/app/src/main/java/com/example/frys/waters/controllers/HistoryGraphActivity.java
+++ Waters/app/src/main/java/com/example/frys/waters/controllers/HistoryGraphActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.frys.waters.R;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HistoryGraphActivity extends AppCompatActivity {
     LineGraphSeries<DataPoint> series;
+    PurityReportDataBaseHandler db = new PurityReportDataBaseHandler(HistoryGraphActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +24,27 @@ public class HistoryGraphActivity extends AppCompatActivity {
         double x,y;
         x = 0;
 
+        Map<String, Integer> list = new HashMap();
+
+        for (int i = 0; i < db.countReport(); i++) {
+            if (list.containsKey(db.getVirusPPM(i))) {
+            }
+        }
+
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < db.countReport(); i++) {
+            String dateAndTime = db.getDateTime(i);
+            String month = dateAndTime.substring(4, 6);
             x = x + 1;
             y = x * x;
             series.appendData(new DataPoint(x, y), true, 10);
         }
 
         graph.addSeries(series);
+        graph.setTitle("Historical Purity Report");
+        graph.getGridLabelRenderer().setHorizontalAxisTitle("month");
+        graph.getGridLabelRenderer().setVerticalAxisTitle("PPM");
+
     }
 }
