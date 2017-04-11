ddc282642193b1e4807871b791dfba6e867d37ed
EllaKim0415 <khrstar7979@gmail.com>
2017-04-10 20:15:26 -0400

    edit profile

--- Waters/app/src/main/java/com/example/frys/waters/controllers/EditProfile.java
+++ Waters/app/src/main/java/com/example/frys/waters/controllers/EditProfile.java
@@ -5,14 +5,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.frys.waters.R;
 import com.example.frys.waters.model.User;
+import com.example.frys.waters.model.WaterPurityReport;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import static com.example.frys.waters.controllers.ChoosePurityHistoryActivity.chooseLocationviewSpinner;
 import static com.example.frys.waters.controllers.LoginActivity.currentUser;
 import static com.example.frys.waters.controllers.LoginActivity.registeredUser;
 
@@ -25,6 +34,9 @@ public class EditProfile extends AppCompatActivity {
     private TextView _username;
     private EditText _name, _email, _password, _confPassword, _address;
     private User user;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
 
     /**
      * OnCreate method required to load activity and loads everything that
@@ -52,12 +64,15 @@ public class EditProfile extends AppCompatActivity {
         _edit_change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //changeProfile();
                 if (_confPassword.getText().toString().equals(_password.getText().toString())) {
                     if (!_name.getText().toString().equals(currentUser.getName())) {
+                        //databaseReference.child("user").child("name").setValue(_name.getText().toString());
                         currentUser.setName(_name.getText().toString());
                     }
                     if (!_email.getText().toString().equals(currentUser.getEmailAddress())) {
-                        currentUser.setEmailAddress(_email.getText().toString());
+                        Toast.makeText(EditProfile.this,"Email cannot be changed",Toast.LENGTH_LONG).show();
+                        //currentUser.setEmailAddress(_email.getText().toString());
                     }
                     if (!_address.getText().toString().equals(currentUser.getHomeAddress())) {
                         currentUser.setHomeAddress(_address.getText().toString());
@@ -81,4 +96,47 @@ public class EditProfile extends AppCompatActivity {
             }
         });
     }
+
+//    public void changeProfile() {
+//        databaseReference.child("purity report").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+//                User childValue;
+//
+//                for (DataSnapshot child : children) {
+//                    if (child.child("emailAddress").getValue().toString().equals(currentUser.getName())) {
+//                        childValue = child.getValue(User.class);
+//                    }
+//                }
+//
+//                if (_confPassword.getText().toString().equals(_password.getText().toString())) {
+//                    if (!_name.getText().toString().equals(currentUser.getName())) {
+//                        //databaseReference.child("user").child("name").setValue(_name.getText().toString());
+//                        currentUser.setName(_name.getText().toString());
+//                    }
+//                    if (!_email.getText().toString().equals(currentUser.getEmailAddress())) {
+//                        Toast.makeText(EditProfile.this,"Email cannot be changed",Toast.LENGTH_LONG).show();
+//                        //currentUser.setEmailAddress(_email.getText().toString());
+//                    }
+//                    if (!_address.getText().toString().equals(currentUser.getHomeAddress())) {
+//                        currentUser.setHomeAddress(_address.getText().toString());
+//                    }
+//                } else {
+//                    Context context = getApplicationContext();
+//                    CharSequence text = "Your password and confirmation does not match.";
+//                    int duration = Toast.LENGTH_SHORT;
+//
+//                    Toast toast = Toast.makeText(context, text, duration);
+//                    toast.show();
+//                }
+//                finish();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 }
